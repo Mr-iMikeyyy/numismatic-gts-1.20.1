8df28d3f9d718a2db20899de1881d462f7c7e144
Mike Mccullough <mikesawesomevids@gmail.com>
2025-09-23 12:01:00 -0400

    Initial commit

---
+++ src/client/java/madmike/numismaticgts/NumismaticGTSClient.java
@@ -0,0 +1,10 @@
+package madmike.numismaticgts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NumismaticGTSClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
